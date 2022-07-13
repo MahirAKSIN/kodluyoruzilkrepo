cc9ba0ff2664e7372d768850109a9470a71ac544
Mahir Aksin <98876295+MahirAKSIN@users.noreply.github.com>
2022-07-13 04:33:16 +0300

    Endpoint and middleware add

--- FinalProject(ArvatoBootcamp)/FinalProject(ArvatoBootcamp)/FinalProject(ArvatoBootcamp)/Program.cs
+++ FinalProject(ArvatoBootcamp)/FinalProject(ArvatoBootcamp)/FinalProject(ArvatoBootcamp)/Program.cs
@@ -40,7 +40,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.UseEndpoints(endpoints =>
-//{
+{
     endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "genres/AllGenres",
